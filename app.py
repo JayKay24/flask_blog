93e2a8b04961564f8824c68b04961b9356c48435
James Kinyua <jameskinyua590@gmail.com>
2017-05-26 20:18:56 +0300

    Create a database connection object in app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 # Import the configuration data.
 from config import Configuration
 
 app = Flask(__name__)
+
 # Use the values from the Configuration object in config.py
 app.config.from_object(Configuration)
+
+# Create an object to manage the database connections.
+db = SQLAlchemy(app)
