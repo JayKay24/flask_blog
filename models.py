969173279669edb3408fe02d256ce3cfa7318eb9
James Kinyua <jameskinyua590@gmail.com>
2017-05-26 21:39:23 +0300

    Create a model Entry

---
+++ models.py
@@ -0,0 +1,33 @@
+import datetime, re
+
+from app import db
+
+# Helper function to generate nice-looking URLs.
+def slugify(s):
+    # Turn a human readable string into a URL separated
+    # by hyphens.
+    return re.sub('[^\w]+', '-', s).lower()
+    
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    slug = db.Column(db.String(100), unique=True)
+    body = db.Column(db.Text)
+    created_timestamp = db.Column(db.Datetime, default=datetime.datetime.now)
+    modified_timestamp = db.Column(db.Datetime, default=datetime.datetime.now,
+                                   onupdate=datetime.datetime.now)
+                                   
+    def __init__(self, *args, **kwargs):
+        # Call parent constructor.
+        super(Entry, self).__init__(*args, **kwargs)
+        # Automatically set the slug based on the title.
+        self.generate_slug()
+        
+    def generate_slug(self):
+        self.slug = ''
+        if self.title:
+            self.slug = slugify(self.title)
+            
+    # Generate a helpful representation of instances of this Entry class.
+    def __repr__(self):
+        return '<Entry: {}>'.format(self.title)
