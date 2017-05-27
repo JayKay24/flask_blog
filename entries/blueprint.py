73fdae00a1be7b1462e0d1fcb486f8b1a52e2d08
James Kinyua <jameskinyua590@gmail.com>
2017-05-27 20:20:06 +0300

    Update detail view function of blueprint.py to render the contents of a single entry

--- entries/blueprint.py
+++ entries/blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 from helpers import object_list
 from models import Entry, Tag
@@ -22,4 +22,9 @@ def tag_detail(slug):
 
 @entries.route('/<slug>/')
 def detail(slug):
-    pass
+    """
+    Render the contents of a single blog entry.
+    """
+    # Return a 404 if none matches.
+    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    return render_template('entries/detail.html', entry=entry)
