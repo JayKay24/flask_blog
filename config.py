1fc0ae2f2d68d8dd05636b11058f1a3140be166c
James Kinyua <jameskinyua590@gmail.com>
2017-05-28 19:05:31 +0300

    Update config.py to include a secret key

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ class Configuration:
     # Provide the path to the database file.
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG=True
+    SECRET_KEY = 'savitar&zoom'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/blog.db'.format(APPLICATION_DIR)
     
