2c4a6d1ec9814bf68615a5d7c730bbc4a01fb9de
James Kinyua <jameskinyua590@gmail.com>
2017-05-30 13:52:09 +0300

    Define subclass EntryModelView that inherits ModelView in admin.py

--- admin.py
+++ admin.py
@@ -7,10 +7,17 @@ from flask.ext.admin.contrib.sqla import ModelView
 from app import app, db
 from models import Entry, Tag, User
 
+class EntryModelView(ModelView):
+    column_list = [
+        'title', 'status', 'author', 'tease', 'tag_list',
+        'created_timestamp'
+    ]
+    column_select_related_list = ['author'] # Efficiently SELECT the author.
+
 # To avoid a circular import, admin is loaded after app.
 admin = Admin(app, 'Blog Admin')
 # Call admin.admin_view and pass instances of the ModelView class
 # as well as the db session, for it to access the database with.
-admin.add_view(ModelView(Entry, db.session))
+admin.add_view(EntryModelView(Entry, db.session))
 admin.add_view(ModelView(Tag, db.session))
 admin.add_view(ModelView(User, db.session))
