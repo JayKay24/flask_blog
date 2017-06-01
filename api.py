0d713242a6a6f60c06498fbd03b6b54b54603a8d
James Kinyua <jameskinyua590@gmail.com>
2017-06-01 08:41:24 +0300

    Define function post_preprocessor in api.py

--- api.py
+++ api.py
@@ -1,6 +1,30 @@
+from flask.ext.restless import ProcessingException
+
 from app import api
+from entries.forms import CommentForm
 from models import Comment
+
+def post_preprocessor(data, **kwargs):
+    """
+    Accept the deserialized POST data as an argument.
+    Validate the submitted coment.
+    """
+    form = CommentForm(data=data)
+    if form.validate():
+        return form.data
+    else:
+        # If validation fails, signal to Flask-Restless that this
+        # data was unprocessable and return a 400 Bad Request response.
+        raise ProcessingException(
+            description='Invalid form submission.',
+            code=400
+        )
 
 # Populate my app with additional URL routes and view code
 # that together, consitutes a RESTFUL API.
-api.create_api(Comment, methods=['GET', 'POST'])
+api.create_api(
+    Comment, 
+    methods=['GET', 'POST'],
+    preprocessors={
+        'POST': [post_preprocessor],    
+    })
